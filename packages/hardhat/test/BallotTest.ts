73d9150c0ea583c6aa576ef255748a987b20f95f
Manu <frosimanuel@gmail.com>
2024-08-04 01:21:04 -0300

    cambios para que no queme tanto gas

--- packages/hardhat/test/BallotTest.ts
+++ packages/hardhat/test/BallotTest.ts
@@ -7,24 +7,29 @@ import { Group, Identity, generateProof } from "@semaphore-protocol/core";
 const PROPOSALS = ["Proposal 1", "Proposal 2", "Proposal 3"];
 
 async function deployContract() {
-  const { semaphore } = await run("deploy:semaphore", {
-    logs: false,
-  });
-  const semaphoreAddress = await semaphore.getAddress();
+  const semaphoreAddress = "0x1e0d7FF1610e480fC93BdEC510811ea2Ba6d7c2f";
   const publicClient = await viem.getPublicClient();
   const [deployer, otherAccount] = await viem.getWalletClients();
 
+  // Deploy Ballot contract
   const ballotContract = await viem.deployContract("Ballot", [
     PROPOSALS.map(prop => toHex(prop, { size: 32 })),
     semaphoreAddress,
   ]);
+
   return { publicClient, deployer, otherAccount, ballotContract };
 }
 
 describe("Ballot", async () => {
+  let deployer, otherAccount, ballotContract, publicClient;
+
+  before(async () => {
+    // Deploy the contract once for all tests
+    ({ publicClient, deployer, otherAccount, ballotContract } = await deployContract());
+  });
+
   describe("when the contract is deployed", async () => {
     it("has the provided proposals", async () => {
-      const { ballotContract } = await loadFixture(deployContract);
       for (let i = 0; i < PROPOSALS.length; i++) {
         const proposalsFromContract = (await ballotContract.read.proposals([BigInt(i)])) as Array<any>;
         const proposal = hexToString(proposalsFromContract[0], { size: 32 });
@@ -33,7 +38,6 @@ describe("Ballot", async () => {
     });
 
     it("has zero votes for all proposals", async () => {
-      const { ballotContract } = await loadFixture(deployContract);
       for (let i = 0; i < PROPOSALS.length; i++) {
         const proposal = (await ballotContract.read.proposals([BigInt(i)])) as Array<bigint>;
         expect(proposal[1]).to.eq(0n);
@@ -43,8 +47,6 @@ describe("Ballot", async () => {
 
   describe("when the voter joins the ballot to vote", async () => {
     it("should be in the voters list with joined status", async () => {
-      const { publicClient, ballotContract, deployer } = await loadFixture(deployContract);
-
       const user = new Identity();
       const tx = await ballotContract.write.joinBallot([user.commitment]);
       const receipt = await publicClient.getTransactionReceipt({ hash: tx });
@@ -58,14 +60,11 @@ describe("Ballot", async () => {
 
   describe("when a valid voter interacts with the vote function in the contract", async () => {
     it("should register the vote", async () => {
-      const { publicClient, ballotContract } = await loadFixture(deployContract);
       const user = new Identity();
       let tx = await ballotContract.write.joinBallot([user.commitment]);
       let receipt = await publicClient.getTransactionReceipt({ hash: tx });
       expect(receipt.status).to.equal("success");
 
-      // Here we use the group with only one member, so it should generate a valid proof on the test
-      // but we should use SemaphoreSubgraph to get the group members when generating the group off-chain
       const group = new Group([user.commitment]);
       const groupId = (await ballotContract.read.groupId()) as number;
       const proposalId = 0;
@@ -82,8 +81,6 @@ describe("Ballot", async () => {
 
   describe("when an user that did not joined the ballot try to vote", async () => {
     it("it should revert", async () => {
-      const { ballotContract, otherAccount } = await loadFixture(deployContract);
-
       const contractAsOtherAccount = await viem.getContractAt("Ballot", ballotContract.address, {
         client: { wallet: otherAccount },
       });
@@ -102,7 +99,6 @@ describe("Ballot", async () => {
 
   describe("when someone interacts with the winningProposal function before any votes are cast", async () => {
     it("should return 0", async () => {
-      const { ballotContract } = await loadFixture(deployContract);
       const winningProposal = await ballotContract.read.winningProposal();
       expect(winningProposal).to.eq(0n);
     });
