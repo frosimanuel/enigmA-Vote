4a99b234e3756fd996a250c092de08d90c1ce56e
Lucianosc <scaminaci.luciano@gmail.com>
2024-08-04 00:04:40 -0300

    add dependencies

--- packages/hardhat/hardhat.config.ts
+++ packages/hardhat/hardhat.config.ts
@@ -55,6 +55,7 @@ const config: HardhatUserConfig = {
     sepolia: {
       url: `https://eth-sepolia.g.alchemy.com/v2/${providerApiKey}`,
       accounts: [deployerPrivateKey],
+      gasPrice: 50000000000,
     },
     arbitrum: {
       url: `https://arb-mainnet.g.alchemy.com/v2/${providerApiKey}`,
